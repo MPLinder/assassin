6e23dde663eb8ac9d30e5ee8760340cfa0ffc23e
Michael Linder <lindercles@gmail.com>
2013-11-07 21:16:45 -0600

    installed south and made initial migration

--- assassin_server/assassin_server/settings.py
+++ assassin_server/assassin_server/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'assassin'
+    'assassin',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
