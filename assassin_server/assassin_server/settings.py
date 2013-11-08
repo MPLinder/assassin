6f3e563729b3b500a9cad396daad3f733b35ff51
Michael Linder <lindercles@gmail.com>
2013-11-07 21:44:51 -0600

    added media url

--- assassin_server/assassin_server/settings.py
+++ assassin_server/assassin_server/settings.py
@@ -76,6 +76,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'smtp.webfaction.com'
 EMAIL_HOST_USER = 'mailbox_username'
